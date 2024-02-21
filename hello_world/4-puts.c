5185b3a0a3077e7c0ca869be70ad5ba6a5232aa8
Aykhanmm <aykhanmm1@gmail.com>
2024-02-21 04:10:16 -0800

    try13

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,5 @@
 #include <studio/h>
-//puts("Programming is like building a multilingual puzzle")
-//return 0;
+int manin () {
+puts("Programming is like building a multilingual puzzle")
+return 0;
+}

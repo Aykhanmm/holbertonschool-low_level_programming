9a328cbc08573c4bd69a614c124ce0ccbc4b12f7
Aykhanmm <aykhanmm1@gmail.com>
2024-02-21 05:25:28 -0800

    try19

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,12 +6,10 @@
  */
 int main(void)
 {
-
-    printf("Size of char: %lu bytes\n", sizeof(char));
+	printf("Size of char: %lu bytes\n", sizeof(char));
     printf("Size of int: %lu bytes\n", sizeof(int));
     printf("Size of long: %lu bytes\n", sizeof(long));
     printf("Size of long long: %lu bytes\n", sizeof(long long));
     printf("Size of float: %lu bytes\n", sizeof(float));
-    
     return 0;
 }
